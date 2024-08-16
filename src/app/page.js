bbfb172b8641a4ace419ca79b379c43bb154073b
Kian Sam <itskiansam@gmail.com>
2024-08-16 00:24:07 -0700

    16.4 Aug

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,12 @@ import Body from "../../components/Body";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
-
 export default function Home() {
   return (
     <section className="m-0 p-0">
       <Navbar />
       <Body />
       <Footer />
-      {/* Hello */}
     </section>
   );
 }
