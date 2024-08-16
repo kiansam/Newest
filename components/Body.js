786b5f5ffee2747b9a5864d1098eb9f28ed4bb73
Kian Sam <itskiansam@gmail.com>
2024-08-16 00:15:52 -0700

    16.2 Aug

--- components/Body.js
+++ components/Body.js
@@ -3,8 +3,8 @@ import React from "react";
 const Body = () => {
   return (
     <div className="top-0">
-      <div className="w-full h-800 bg-gradient-to-b from-white to-gray-300 justify-center text-center pt-8">
-        <h1 className="justify-center flex text-5xl font-sans font-bold align-middle">
+      <div className="w-full h-800 justify-center text-center align-middle flex pt-96">
+        <h1 className="justify-center flex text-3xl font-sans font-bold align-middle">
           Kian Samadani
         </h1>
       </div>
