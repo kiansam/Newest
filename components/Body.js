fa9d1d40f88a2451d7eff30936d5edcf4ed2a661
Kian Sam <itskiansam@gmail.com>
2024-12-02 12:09:02 -0800

    Dec 2

--- components/Body.js
+++ components/Body.js
@@ -8,14 +8,7 @@ const Body = () => {
           Kian Samadani
         </h1> */}
         <h1 className="justify-center flex xl:w-5/12 lg:w-8/12 md:w-10/12 w-11/12 text-justify text-md md:text-lg lg:text-xl font-serif p-6 sm:p-12 md:p-16 lg:p-20 xl:p-24 border border-gray-50 rounded-xl shadow-md">
-          "O Ye People that Have Minds to Know and Ears to Hear! The first call
-          of the Beloved is this: O mystic nightingale! Abide not but in the
-          rose garden of the spirit. O messenger of the Solomon of love! Seek
-          thou no shelter except in the Sheba of the well-beloved, and O
-          immortal phoenix! dwell not save on the mount of faithfulness. Therein
-          is thy habitation, if on the wings of thy soul thou soarest to the
-          realm of the infinite and seekest to attain thy goal." (The Hidden
-          Words)
+          KS{" "}
         </h1>
       </div>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,30 @@ import Image from "next/image";
 import Body from "../../components/Body";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen justify-between">
       <Navbar />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1335079440288698"
+        crossorigin="anonymous"
+      />
+      <Script id="adsbygoogle-init">
+        {`(adsbygoogle = window.adsbygoogle || []).push({})`}
+      </Script>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1335079440288698"
+        data-ad-slot="9717105356"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
       <Body />
-      <Footer className="" />
+      <Footer />
     </div>
   );
 }
