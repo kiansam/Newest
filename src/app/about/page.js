d4a4dc811381797b7f78b0878b24bea5803a1576
Kian Sam <itskiansam@gmail.com>
2024-12-02 12:14:59 -0800

    Dec 2.2

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,11 +2,27 @@ import Image from "next/image";
 import Body from "../../../components/About";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
+import Script from "next/script";
 
 export default function About() {
   return (
     <div className="flex flex-col h-screen justify-between">
       <Navbar />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1335079440288698"
+        crossorigin="anonymous"
+      ></Script>
+
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-1335079440288698"
+        data-ad-slot="7226673389"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <Script>{`(adsbygoogle = window.adsbygoogle || []).push({})`}</Script>
       <Body />
       <Footer className="" />
     </div>
